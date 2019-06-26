6eec75884c57f91f5a5a1759e9d05be44d1ab680
Michael Villarreal <mikev8492@gmail.com>
2019-06-26 01:11:51 -0500

    working

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const app = express();
 const request = require('request');
 app.set('view engine', 'ejs');
 
+
+
+
 app.get('/', function(req, res){
   res.render('search');
 });
 
 app.get('/results', function(req, res){
-  request("http://omdbapi.com/?s=iowa&apikey=thewdb", function(error, response, body){
+  let query = req.query.search;
+  let url = 'http://omdbapi.com/?s=';
+  let key = "&apikey=thewdb";
+
+  request(url + query + key, function(error, response, body){
     if(!error && response.statusCode == 200){
       var data = JSON.parse(body);
       res.render('results', {data: data});
